6bf0ec63a1e15a8fbe568affded806dc5f0dc1a1
ThePlayer571 <ThePlayer571@qq.com>
2025-06-19 23:23:42 +0800

    二段跳 一些小更改

--- Scripts/ViewControllers/Player.cs
+++ Scripts/ViewControllers/Player.cs
@@ -8,6 +8,7 @@ namespace TPL.PVZR.ViewControllers
     {
         private PlayerInputControl _inputActions;
         private Rigidbody2D _Rigidbody;
+        private Collider2D _Collider;
 
         [SerializeField] private float speed;
         [SerializeField] private float jumpForce;
@@ -27,16 +28,40 @@ namespace TPL.PVZR.ViewControllers
             _Rigidbody.AddForce(dragForce);
         }
 
+
+        private bool _hasTwiceJumped = false; // 已经进行二段跳
+
+        private void Jump()
+        {
+            var start = new Vector2(this.transform.position.x, _Collider.bounds.min.y);
+            var size = new Vector2(0.8f, 0.02f);
+            bool OnGround = Physics2D.OverlapBox(start, size, 0, LayerMask.GetMask("Barrier"));
+            
+            if (OnGround)
+            {
+                _hasTwiceJumped = false;
+                _Rigidbody.velocity = new Vector2(_Rigidbody.velocity.x, jumpForce);
+            }
+            else if (!_hasTwiceJumped)
+            {
+                _hasTwiceJumped = true;
+                _Rigidbody.velocity = new Vector2(_Rigidbody.velocity.x, jumpForce);
+            }
+            else // 已经二段跳，不能跳了
+            {
+                // do nothing
+            }
+        }
+
         private void Awake()
         {
+            _Rigidbody = this.GetComponent<Rigidbody2D>();
+            _Collider = this.GetComponent<Collider2D>();
+
             _inputActions = new PlayerInputControl();
             _inputActions.Level.Enable();
 
-            _inputActions.Level.Jump.performed += (context) =>
-            {
-                _Rigidbody.velocity = new Vector2(_Rigidbody.velocity.x, jumpForce);
-            };
-            _Rigidbody = this.GetComponent<Rigidbody2D>();
+            _inputActions.Level.Jump.performed += (context) => { Jump(); };
         }
     }
 }
